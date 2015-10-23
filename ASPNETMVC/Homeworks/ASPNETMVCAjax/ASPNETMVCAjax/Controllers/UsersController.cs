b46048351adf0b80ac731387064c132dd81ca977
ivan <ivan4otopa@abv.bg>
2015-10-23 19:30:11 +0300

    Added ASPNETMVCAjax homework

---
+++ ASPNETMVC/Homeworks/ASPNETMVCAjax/ASPNETMVCAjax/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿namespace ASPNETMVCAjax.Controllers
+{
+    using Models;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    public class UsersController : Controller
+    {
+        public JsonResult CheckUsername(string username)
+        {
+            var db = new ApplicationDbContext();
+            var data = db.Users
+                .FirstOrDefault(u => u.UserName == username);
+
+            return Json(data == null, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ASPNETMVC/Homeworks/ASPNETMVCAjax/ASPNETMVCAjax/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPNETMVCAjax.Startup))]
+namespace ASPNETMVCAjax
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
