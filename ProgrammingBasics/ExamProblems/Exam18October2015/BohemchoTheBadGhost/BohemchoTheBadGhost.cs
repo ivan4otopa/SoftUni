d48ae9c156ea8aff6f7dd85bd8e15589c31e4006
ivan <ivan4otopa@abv.bg>
2015-11-09 14:58:54 +0200

    Added ProgrammingBasics Exam18October2015

---
+++ ProgrammingBasics/ExamProblems/Exam18October2015/BohemchoTheBadGhost/BohemchoTheBadGhost.cs
@@ -0,0 +1,60 @@
+﻿namespace BohemchoTheBadGhost
+{
+    using System;
+
+    class BohemchoTheBadGhost
+    {
+        static void Main()
+        {
+            string input = string.Empty;
+            string binaryFloor = "".PadLeft(32, '0');
+            int currentApartment = 0;
+            long totalScore = 0;
+            int totalLightsOn = 0;
+
+            while (true)
+            {
+                input = Console.ReadLine();
+
+                if (input == "Stop, God damn it")
+                {
+                    break;
+                }
+                else
+                {
+                    binaryFloor = Convert.ToString(long.Parse(input), 2).PadLeft(32, '0');
+                }
+
+                string[] apartments = Console.ReadLine().Split(' ');
+
+                foreach (var apartment in apartments)
+                {
+                    currentApartment = int.Parse(apartment);
+
+                    if (binaryFloor[31 - currentApartment] == '0')
+                    {
+                        binaryFloor = binaryFloor.Remove(31 - currentApartment, 1).Insert(31 - currentApartment, "1");
+                    }
+                    else
+                    {
+                        binaryFloor = binaryFloor.Remove(31 - currentApartment, 1).Insert(31 - currentApartment, "0");
+                    }
+                }
+
+                for (int i = 0; i < binaryFloor.Length; i++)
+                {
+                    if (binaryFloor[i] == '1')
+                    {
+                        totalLightsOn++;
+                    }
+                }
+
+                totalScore += Convert.ToInt64(binaryFloor, 2);
+
+                binaryFloor = "".PadLeft(32, '0');
+            }
+
+            Console.WriteLine("Bohemcho left {0} lights on and his score is {1}", totalLightsOn, totalScore);            
+        }
+    }
+}
---
+++ ProgrammingBasics/ExamProblems/Exam18October2015/GrandTheftExamo/GrandTheftExamo.cs
@@ -0,0 +1,44 @@
+﻿namespace GrandTheftExamo
+{
+    using System;
+    using System.Numerics;
+
+    class GrandTheftExamo
+    {
+        static void Main()
+        {
+            int escapeAttempts = int.Parse(Console.ReadLine());
+            int thieves = 0;
+            int beers = 0;
+            int thievesSlapped = 0;
+            BigInteger thievesEscaped = 0;
+            int attemptThievesEscaped = 0;
+            BigInteger totalBeers = 0;
+            BigInteger beerPackSpace = 6;
+
+            for (int i = 0; i < escapeAttempts; i++)
+            {
+                thieves = int.Parse(Console.ReadLine());
+                beers = int.Parse(Console.ReadLine());
+
+                if (thieves <= 5)
+                {
+                    thievesSlapped += thieves;
+                }
+                else
+                {
+                    attemptThievesEscaped = thieves - 5;
+                    thievesEscaped += attemptThievesEscaped;
+                    thievesSlapped += thieves - attemptThievesEscaped;
+                }
+
+                totalBeers += beers;
+            }
+
+            BigInteger beerPacks = totalBeers / beerPackSpace;
+            BigInteger beerBottles = totalBeers % beerPackSpace;
+
+            Console.WriteLine("{0} thieves slapped.\n{1} thieves escaped.\n{2} packs, {3} bottles.", thievesSlapped, thievesEscaped, beerPacks, beerBottles);
+        }
+    }
+}
