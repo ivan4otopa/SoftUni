6adf9ee1c59b2d416d3c52c9b77c58fd4cc70bfa
ivan <ivan4otopa@abv.bg>
2015-12-05 14:10:44 +0200

    Added C#Basics 10April exams

---
+++ C#Basics/Exams/Exam10April2014/BitsInverter/BitsInverter.cs
@@ -0,0 +1,42 @@
+﻿namespace BitsInverter
+{
+    using System;
+
+    class BitsInverter
+    {
+        static void Main()
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            int step = int.Parse(Console.ReadLine());
+            string combinedBits = string.Empty;
+            int number = 0;
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                number = int.Parse(Console.ReadLine());
+                combinedBits += Convert.ToString(number, 2).PadLeft(8, '0');
+            }
+
+            for (int i = 0; i < combinedBits.Length; i += step)
+            {
+                if (combinedBits[i] == '0')
+                {
+                    combinedBits = combinedBits.Remove(i, 1).Insert(i, "1");
+                }
+                else
+                {
+                    combinedBits = combinedBits.Remove(i, 1).Insert(i, "0");
+                }
+            }
+
+            string bits = string.Empty;
+
+            for (int i = 0; i < combinedBits.Length; i += 8)
+            {
+                bits = combinedBits.Substring(i, 8);
+
+                Console.WriteLine(Convert.ToByte(bits, 2));
+            }
+        }
+    }
+}
---
+++ C#Basics/Exams/Exam10AprilVariant2/HayvanNumbers/HayvanNumbers.cs
@@ -0,0 +1,68 @@
+﻿namespace HayvanNumbers
+{
+    using System;
+
+    class HayvanNumbers
+    {
+        static void Main()
+        {
+            int sum = int.Parse(Console.ReadLine());
+            int difference = int.Parse(Console.ReadLine());
+            int hayvanNumbers = 0;
+            int secondNumber = 0;
+            int thirdNumber = 0;
+            string number = string.Empty;
+
+            for (int i = 555; i <= 999 - 2 * difference; i++)
+            {
+                secondNumber = i + difference;
+                thirdNumber = i + 2 * difference;
+                number =
+                    i.ToString() +
+                    secondNumber.ToString() +
+                    thirdNumber.ToString();
+
+                if (DigitsInRangeCheck(i) && DigitsInRangeCheck(secondNumber) &&
+                    DigitsInRangeCheck(thirdNumber) && 
+                    SumDigits(int.Parse(number)) == sum)
+                {
+                    Console.WriteLine(number);
+
+                    hayvanNumbers++;
+                }
+            }
+
+            if (hayvanNumbers == 0)
+            {
+                Console.WriteLine("No");
+            }
+        }
+
+        static int SumDigits(int number)
+        {
+            int digitSum = 0;
+
+            while (number > 0)
+            {
+                digitSum += number % 10;
+                number /= 10;
+            }
+
+            return digitSum;
+        }
+
+        static bool DigitsInRangeCheck(int number)
+        {
+            int firstDigit = number / 100;
+            int secondDigit = (number % 100) / 10;
+            int thirdDigit = number % 10;
+
+            if (firstDigit >= 5 && secondDigit >= 5 && thirdDigit >= 5)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
