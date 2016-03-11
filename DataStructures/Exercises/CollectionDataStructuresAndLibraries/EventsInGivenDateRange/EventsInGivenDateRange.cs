a8ec97b56d76ca82862875328fc179e044d9427e
ivan <ivan4otopa@abv.bg>
2016-03-11 14:26:54 +0200

    Added exercise

---
+++ DataStructures/Exercises/CollectionDataStructuresAndLibraries/EventsInGivenDateRange/EventsInGivenDateRange.cs
@@ -0,0 +1,57 @@
+﻿namespace EventsInGivenDateRange
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Threading;
+
+    using Wintellect.PowerCollections;
+
+    class EventsInGivenDateRange
+    {
+        static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            var events = new OrderedMultiDictionary<DateTime, string>(true);
+            int linesCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < linesCount; i++)
+            {
+                string eventEntry = Console.ReadLine();
+                var eventTokens = eventEntry.Split('|');
+                string eventName = eventTokens[0].Trim();
+
+                DateTime eventDate = DateTime.Parse(eventTokens[1].Trim());
+
+                events.Add(eventDate, eventName);
+            }
+
+            var dateRanges = new Dictionary<DateTime, DateTime>();
+            int rangesCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rangesCount; i++)
+            {
+                string rangeEntry = Console.ReadLine();
+                var rangeTokens = rangeEntry.Split('|');
+
+                DateTime startDate = DateTime.Parse(rangeTokens[0].Trim());
+                DateTime endDate = DateTime.Parse(rangeTokens[1].Trim());
+
+                dateRanges.Add(startDate, endDate);
+            }
+
+            foreach (var range in dateRanges)
+            {
+                var eventsInRange = events.Range(range.Key, true, range.Value, true);
+
+                Console.WriteLine(eventsInRange.KeyValuePairs.Count);
+
+                foreach (var eventName in eventsInRange.Values)
+                {
+                    Console.WriteLine("{0} | {1}", eventName, range.Key);
+                }
+            }
+        }
+    }
+}
---
+++ DataStructures/Exercises/CollectionDataStructuresAndLibraries/RopeForEfficientStringEditing/RopeForEfficientStringEditing.cs
@@ -0,0 +1,84 @@
+﻿namespace RopeForEfficientStringEditing
+{
+    using System;
+
+    using Wintellect.PowerCollections;
+
+    class RopeForEfficientStringEditing
+    {
+        static BigList<char> stringRope = new BigList<char>();
+
+        static void Main()
+        {
+            string command = Console.ReadLine();
+            string commandName = string.Empty;
+            int startIndex = 0;
+            string text = string.Empty;
+            int count = 0;
+
+            while (command != "PRINT")
+            {
+                var commandTokens = command.Split(' ');
+                
+                commandName = commandTokens[0].Trim();
+
+                switch (commandName)
+                {
+                    case "INSERT":
+                        text = commandTokens[1].Trim();
+                        Console.WriteLine(InsertString(text));
+
+                        break;
+                    case "APPEND":
+                        text = commandTokens[1].Trim();
+                        Console.WriteLine(AppendString(text));
+
+                        break;
+                    case "DELETE":
+                        startIndex = int.Parse(commandTokens[1].Trim());
+                        count = int.Parse(commandTokens[2].Trim());
+                        Console.WriteLine(DeleteString(startIndex, count));
+
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join("", stringRope));
+        }
+
+        static string InsertString(string text)
+        {
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                stringRope.AddToFront(text[i]);
+            }
+
+            return "OK";
+        }
+
+        static string AppendString(string text)
+        {
+            stringRope.AddRange(text);
+
+            return "OK";
+        }
+
+        static string DeleteString(int startIndex, int count)
+        {
+            if (startIndex < 0 || startIndex > stringRope.Count ||
+                startIndex + count > stringRope.Count)
+            {
+                return "ERROR";
+            }
+
+            for (int i = startIndex; i < startIndex + count; i++)
+            {
+                stringRope.RemoveAt(startIndex);
+            }
+
+            return "OK";
+        }
+    }
+}
